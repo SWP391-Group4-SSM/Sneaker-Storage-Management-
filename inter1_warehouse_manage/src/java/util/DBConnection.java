0bec79d95afd60f888a0e9357989280cf4447f7f
Nguyen Trung Nghia <nghianthe150495@fpt.edu.vn>
2025-02-17 10:19:29 +0700

    merge from warehouse
    Last update: Vuong + Duc

--- inter1_warehouse_manage/src/java/util/DBConnection.java
+++ inter1_warehouse_manage/src/java/util/DBConnection.java
@@ -1,3 +1,4 @@
+<<<<<<< Updated upstream:inter1_warehouse_manage/src/java/util/DBConnection.java
 package util;
 
 import java.sql.Connection;
@@ -42,4 +43,50 @@ public class DBConnection {
 //            return null;
 //        }
 //    }
+=======
+package util;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class DBConnection {
+    public static Connection getConnection() {
+    try {
+        Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
+        String url = "jdbc:sqlserver://localhost:1433;databaseName=SneakerManagement;encrypt=false;trustServerCertificate=true";
+        String user = "sa";
+        String password = "123456";
+        return DriverManager.getConnection(url, user, password);
+    } catch (SQLException | ClassNotFoundException e) {
+        System.err.println("Database connection failed: " + e.getMessage());
+        throw new RuntimeException("Cannot connect to database", e);
+    }
+}
+
+}
+
+
+
+//package util;
+//
+//import java.sql.Connection;
+//import java.sql.DriverManager;
+//import java.sql.SQLException;
+//
+//public class DBConnection {
+//    public static Connection getConnection() {
+//        try {
+//            Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
+//            String url = "jdbc:sqlserver://localhost:1433;databaseName=SneakerManagement;encrypt=false;trustServerCertificate=true";
+//            String user = "sa";
+//            String password = "123456789";
+//            return DriverManager.getConnection(url, user, password);
+//        } catch (SQLException | ClassNotFoundException e) {
+//            System.err.println("Error " + e.getMessage() + " at DB Context");
+//            e.printStackTrace();
+//            return null;
+//        }
+//    }
+>>>>>>> Stashed changes:src/java/util/DBConnection.java
 //}
